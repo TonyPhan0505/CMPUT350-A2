d056e9b43dc8bba731951967544d4e8652930fb0
B Phan <bphan@Bs-MacBook-Pro.local>
2023-10-16 23:05:18 -0600

    implemented constructor for marines

--- Marine.cpp
+++ Marine.cpp
@@ -2,5 +2,7 @@
 
 // ... implement
 Marine :: Marine(Team team, const Vec2 &pos, AttackPolicy pol, bool bounce) : Unit(team, pos, 10, 40, 10, 1, 5) {
-    
+    this -> policy = pol;
+    this -> bounce = bounce;
 }
+
